50ec825266eaafe74331db88c7c024ca34944bc3
Lucas <lucas.contreras@mercadolibre.com>
2020-07-22 14:17:07 -0300

    deplot fix

--- terraform/7_s3_static_stie/deploy.sh
+++ terraform/7_s3_static_stie/deploy.sh
@@ -1,1 +1,1 @@
-aws s3 sync --exclude="*infra*" --exclude="*.git*" --exclude="*.idea*" . s3://transporterugiero.com/
+aws s3 sync --exclude="*infra*" --exclude="*.git*" --exclude="*.idea*" . s3://workshopmeli.ml/
