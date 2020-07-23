92d9e37929409ddc5ea02acf6582a729ee3a92c2
Lucas Calisi <lucas.calisi@mercadolibre.com>
2020-07-23 19:38:51 -0300

    :cow:

--- terraform/6_importing_existing_resources/instance.tf
+++ terraform/6_importing_existing_resources/instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "test" {
-  ami= "ami-0affd4508a5d2481b"
-  instance_type = "t2.micro"
+  ami                  = "ami-0affd4508a5d2481b"
+  instance_type        = "t2.micro"
   iam_instance_profile = "CloudSec-SSM-EC2-EC2DefaultRoleInstanceProfile"
 }
