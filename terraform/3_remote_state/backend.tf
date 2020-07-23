043b6bb4b8cb02cf32f2de043960e999296a0f1f
Lucas Calisi <lucas.calisi@mercadolibre.com>
2020-07-23 18:46:25 -0300

    :lion:

--- terraform/3_remote_state/backend.tf
+++ terraform/3_remote_state/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "workshop-terraform-cloudsec-states"
-    key    = "workshop_status.tf"
+    bucket = ""
+    key    = "terraform.tfstate"
     region = "us-east-1"
   }
 }
