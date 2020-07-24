65b6b198c543f58ad485f859b8b6cf9fadb240a9
Lucas <lucas.contreras@mercadolibre.com>
2020-07-24 17:23:41 -0300

    Ejericio referencias

---
+++ terraform/exercises/0_fix_instance_template/sg.tf
@@ -0,0 +1,26 @@
+# AWS EC2 Security group for MasterTV
+resource "aws_security_group" "not-a-webserver-sg" {
+  name = "workshop-iac-sg"
+  description = "Allow https/http/ssh ports"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
