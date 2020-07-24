5c20f36add4b866a5ba0d844732cfc121913b228
Lucas <lucas.contreras@mercadolibre.com>
2020-07-24 17:31:32 -0300

    Test Populate Controller 2

--- terraform/exercises/0_fix_instance_template/variables.tf
+++ terraform/exercises/0_fix_instance_template/variables.tf
@@ -1,17 +1,4 @@
-variable "AMIS" {
-  type = map(string)
-  default = {
-    us-east-1 = "ami-000b3a073fc20e415"
-  }
-}
-
 variable "AWS_REGION" {
   default = "us-east-1"
 }
 
-variable "tags" {
-  default = {
-    "team"    = "cloudsec"
-    "project" = "workshop-iac"
-  }
-}
